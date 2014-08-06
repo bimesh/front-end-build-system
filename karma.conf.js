c225bda19600231e346fb01d23ddc8f70dba3990
ctesene-accela <ctesene@accela.com>
2014-08-06 14:51:06 -0700

    front end code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -2,61 +2,74 @@ module.exports = function (config) {
   'use strict';
 
   config.set({
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+      // enable / disable watching file and executing tests whenever any file changes
+      autoWatch: true,
 
-    // base path, that will be used to resolve files and exclude
-    // basePath: '../',
+      // base path, that will be used to resolve files and exclude
+      // basePath: '../',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+      // testing framework to use (jasmine/mocha/qunit/...)
+      frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'app/lib/**/*.js',
-      'app/js/**/*.js',
-      'app/tests/**/*.js'
-    ],
+      // list of files / patterns to load in the browser
+      files: [
+        'app/lib/**/*.js',
+        'app/js/**/*.js',
+        'app/tests/**/*.js'
+      ],
 
-    // list of files / patterns to exclude
-    exclude: [],
+      preprocessors: {
+        'app/js/**/*.js': 'coverage'
+      },
 
-    // web server port
-    port: 8080,
+      // list of files / patterns to exclude
+      exclude: [],
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: [
-      'PhantomJS'
-    ],
+      // web server port
+      port: 8080,
 
-    // Which plugins to enable
-    plugins: [
-      'karma-phantomjs-launcher',
-      'karma-jasmine'
-    ],
+      // Start these browsers, currently available:
+      // - Chrome
+      // - ChromeCanary
+      // - Firefox
+      // - Opera
+      // - Safari (only Mac)
+      // - PhantomJS
+      // - IE (only Windows)
+      browsers: [
+        'PhantomJS'
+      ],
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false,
+      // Which plugins to enable
+      plugins: [
+        'karma-phantomjs-launcher',
+        'karma-jasmine',
+        'karma-coverage'
+      ],
 
-    colors: true,
+      reporters: ['coverage'],
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO
+      coverageReporter: {
+        type: 'html',
+        dir: 'coverage/'
+      },
 
-    // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
-    // URL root prevent conflicts with the site root
-    // urlRoot: '_karma_'
-  });
+      // Continuous Integration mode
+      // if true, it capture browsers, run tests and exit
+      singleRun: false,
+
+      colors: true,
+
+      // level of logging
+      // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+      logLevel: config.LOG_INFO
+
+      // Uncomment the following lines if you are using grunt's server to run the tests
+      // proxies: {
+      //   '/': 'http://localhost:9000/'
+      // },
+      // URL root prevent conflicts with the site root
+      // urlRoot: '_karma_'
+    }
+  );
 };
