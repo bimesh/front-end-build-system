b2114231fd02f8834fc713511d0870d1d38e049e
ctesene-accela <ctesene@accela.com>
2014-08-06 12:39:03 -0700

    javascript unit testing

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ front-end-build-system
 
 * Get familiar with Gulp
   * https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md
+* Get familiar with Karma
+  * http://karma-runner.github.io/0.12/intro/installation.html
+    
---
+++ tests/app.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Controller: AboutCtrl', function () {
+
+  // load the controller's module
+  beforeEach(function() {
+
+  });
+
+  it('should have a global object name Automation', function () {
+    expect(Automation).not.toBeUndefined();
+  });
+});
